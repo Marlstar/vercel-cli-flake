9963b998d530b8d079da0f94bcec88911c08f401
Marley Reeves <marley@marlstar.com>
2026-05-27 08:47:32 +1000

    feat: vercel cli flake

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+	description = "Vercel CLI (native binary)";
+
+	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+	outputs = { self, nixpkgs }: let
+	version = "54.4.1";
+
+	platforms = {
+		"x86_64-linux" = {
+			pkg = "vc-native-linux-x64";
+			hash = "sha512-0e/1SZKvk/mh1r1LfMahn5atJ4T7zs3pxAXz7bp/dR5CjB5lul/CrKdhDt6b/iidisjw92aMKxORJT6zbx2sbA==";
+		};
+		"aarch64-linux" = {
+			pkg = "vc-native-linux-arm64";
+			hash = "sha512-IJxEO7dm/ZVRiza0/fJfzEQoRrsUfAgOyPOs56OzLsBcVOW+54VQ09jxNagWNEnOcGHM/IWzxWIcU+pcLX3uPw==";
+		};
+	};
+
+	forAllSystems = nixpkgs.lib.genAttrs (builtins.attrNames platforms);
+	in {
+		packages = forAllSystems (system: let
+		pkgs = nixpkgs.legacyPackages.${system};
+		meta = platforms.${system};
+		in {
+			default = pkgs.stdenv.mkDerivation {
+				pname = "vercel";
+				inherit version;
+
+				src = pkgs.fetchurl {
+					url = "https://registry.npmjs.org/@vercel/${meta.pkg}/-/${meta.pkg}-${version}.tgz";
+					hash = meta.hash;
+				};
+
+				nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+				buildInputs = [ pkgs.stdenv.cc.cc.lib ];
+
+				installPhase = ''
+				mkdir -p $out/bin
+				cp bin/vercel $out/bin/vercel
+				chmod +x $out/bin/vercel
+				'';
+
+				meta = {
+					description = "The command-line interface for Vercel (native binary)";
+					homepage = "https://vercel.com";
+					license = pkgs.lib.licenses.asl20;
+					mainProgram = "vercel";
+					platforms = builtins.attrNames platforms;
+				};
+			};
+		}
+		);
+	};
+}
